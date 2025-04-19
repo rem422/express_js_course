1ddfa15d05c9badb9dcdfde1dcef6342ac04324e
Rem Simiyu <remsimiyu@gmail.com>
2025-04-19 23:57:49 +0300

    Backend with Express js

---
+++ src/index.mjs
@@ -0,0 +1,13 @@
+// creating a server in express
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
+
+//
+
